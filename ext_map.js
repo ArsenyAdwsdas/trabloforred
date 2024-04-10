f391a443f81e3d2d9998ca69987fb7fa385096e8
ArsenyAdwsdas <61195101+ArsenyAdwsdas@users.noreply.github.com>
2024-04-10 14:42:03 +0300

    Create ext_map.js

---
+++ ext_map.js
@@ -0,0 +1,34 @@
+const md=new(require('markdown-it'))({html:true});
+const fs=require('fs');
+//the victim .html to replace stuff for md
+const templatePath="public/md.html";
+//lists all that gets replaced once to make page assembly faster(probably)
+const splitrs=["{[title]}","{[head]}","{[transit1]}","{[content]}","{[transit2]}","{[foot]}"]
+
+const form=[]{
+	const s=fs.readFileSync(templatePath).toString(),I=0
+	for(let i=0;i<splitrs.length;i++){
+		const _=s.indexOf(splitrs[i],I)
+		if(I>_){console.log(`Search for "${splitrs[i]}" failed.`);splitrs.splice(i,1);i--}
+		else{form.push(s.slice(_));I=_+splitrs[i].length}
+	}form.push(s.slice(I))
+}
+module.exports={
+	md:{
+		ext:"html",
+		cast=(s)=>{
+			let cont=fs.readFileSync(file).toString()
+			//what to replace stuff with
+			let o={
+				"{[title]}":cont.startsWith('# ')?cont.slice(2).replace(/\n.+/s,""):"&lt;untitled&gt;",
+				"{[head]}":"<a href=\"/\">Main page</a>",
+				"{[transit1]}":"",
+				"{[content]}":md.render(cont),
+				"{[transit2]}":"",
+				"{[foot]}":""
+			}
+			let l=[form[0]];for(let i=0;i<splitrs.length;i++)l.push(o[splitrs[i]]!=undefined?o[splitrs[i]]:"&lt;undefined&gt;",form[i+1])
+			return l.join("")
+		}
+	}
+}
